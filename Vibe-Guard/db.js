eacde2382442a5660d7a6868a1ca60a3d41f7186
HaleemaUsman19 <sp23-bse-070@cuilahore.edu.pk>
2025-08-26 21:03:59 +0500

    last changings before deploying

--- Vibe-Guard/db.js
+++ Vibe-Guard/db.js
@@ -1,16 +1,14 @@
 const mongoose = require("mongoose");
 
-const connectdb = async () =>{
+const connectdb = async () => {
+    const connectionString = process.env.MONGO_URI; // use env variable
 
-    const connectionString = "mongodb+srv://eeshaansar:vibeguard@vg.h1qaw.mongodb.net/?retryWrites=true&w=majority&appName=VG";
-
-    try{
+    try {
         await mongoose.connect(connectionString);
-        console.log("Connect to MongoDb");
-    }
-    catch(error){
+        console.log("Connected to MongoDB");
+    } catch (error) {
         console.error("MongoDB connection error:", error.message);
-        process.exit(1); 
+        process.exit(1);
     }
 };
 
